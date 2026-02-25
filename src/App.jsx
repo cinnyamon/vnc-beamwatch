61a3c459af5d2ee8ddd475edecfa77ad77f6ea4b
cinny <cinnyamonn@gmail.com>
2026-02-25 23:16:56 +0100

    small container start for beamwatch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "./components/Dash";
+import "./main.css";
+
+function App() {
+  return (
+    <>
+      <Dashboard></Dashboard>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Dash.jsx
@@ -0,0 +1,12 @@
+const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-12 h-full gap-4 p-4">
+      <div className="glass col-span-9 p-4 rounded-2xl">
+        <iframe src="" frameborder="0"></iframe>
+      </div>
+      <div className="glass col-span-3 p-4 rounded-2xl">aaa</div>
+    </div>
+  );
+};
+
+export default Dashboard;
